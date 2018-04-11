f40cca948199299f156afc41d313d0cb3e5f177a
Qimu Zheng <zheng.qm@163.com>
2018-04-11 14:28:30 +0800

    visualization 70% done

--- module_metric_generator.py
+++ module_metric_generator.py
@@ -5,7 +5,7 @@ from maintainer_file_parser import *
 from module_metric_calculator import *
 from pymongo import MongoClient
 
-def insert_metric(module_name, metric_name, metric, db):
+def insert_metric(module_name, metric_name, metric, n_maitainers, db):
     x = list(metric.index.map(lambda idx: str(idx[0]) + '.' + str(idx[1])))
     y = map(lambda n: float(n), list(metric))
     data = {
@@ -14,11 +14,12 @@ def insert_metric(module_name, metric_name, metric, db):
     }
     db[module_name].insert_one({
         'name': metric_name,
+        'n'   : n_maitainers,
         'data': data
         })
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG, filename='module_metric.log')
 
     client = MongoClient()
     db = client.research
@@ -34,30 +35,38 @@ if __name__ == '__main__':
     logging.info("MAINTAINER info loaded")
 
     for module_name, info in modules.items():
-        if 'M' not in info:
-            continue
-        if 'F' not in info:
-            continue
-        if ('S' not in info) or (info['S'][0] not in ['Supported', 'Maintained']):
-            continue
-            
-        logging.info("Dropping all documents inside collection " + module_name)
-        db[module_name].drop()
+        try:
+            if 'M' not in info:
+                continue
+            if 'F' not in info:
+                continue
+            if ('S' not in info) or (info['S'][0] not in ['Supported', 'Maintained']):
+                continue
+                
+            logging.info("Processing collection " + module_name)
+            db[module_name].drop()
 
-        n_maitainers = len(info['M'])
-        calculators = {
-            'total_commit_count': total_commit_count,
-            'top_n_commit_count': lambda df: top_n_commit_count(df, n_maitainers),
-            'top_n_commit_ratio': lambda df: top_n_commit_ratio(df, n_maitainers),
-            'latency_median': latency_median,
-            'latency_mean': latency_mean,
-            'non_contiguous_pressure': non_contiguous_pressure,
-            'contiguous_pressure': contiguous_pressure,
-            'max_file_complexity': max_file_complexity,
-            'max_contact_complexity': max_contact_complexity,
-        }
-        commits = commit_filter(df, info)
-        for metric_name, calculator in calculators.items():
-            logging.info("Module: " + module_name + " Metric: " + metric_name)
-            metric = calculator(commits)
-            insert_metric(module_name, metric_name, metric, db)
+            n_maitainers = len(info['M'])
+            calculators = {
+                'total_commit_count': total_commit_count,
+                'top_n_commit_count': lambda df: top_n_commit_count(df, n_maitainers),
+                'top_n_commit_ratio': lambda df: top_n_commit_ratio(df, n_maitainers),
+                'latency_median': latency_median,
+                'latency_mean': latency_mean,
+                'non_contiguous_pressure': non_contiguous_pressure,
+                'contiguous_pressure': contiguous_pressure,
+                'max_file_complexity': max_file_complexity,
+                'max_contact_complexity': max_contact_complexity,
+            }
+            commits = commit_filter(df, info)
+            for metric_name, calculator in calculators.items():
+                #logging.info("Module: " + module_name + " Metric: " + metric_name)
+                metric = calculator(commits)
+                insert_metric(module_name, metric_name, metric, n_maitainers, db)
+        except Exception, e:
+            logging.warning("In module " + module_name)
+            logging.warning(info)
+            logging.warning(e)
+            continue
+
+
--- web/demo.py
+++ web/demo.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from flask import render_template
+from flask import *
 from pymongo import MongoClient
+from collections import defaultdict
 
 app = Flask(__name__)
+
+app.config.update(dict(
+    SECRET_KEY='development key',
+))
 
 @app.route('/')
 def index():
@@ -28,12 +32,73 @@ def index():
         'mean' : u'均值',
         'median' : u'中值',
     }
+
+    collections = db.collection_names()
+    if 'overall' in collections:
+        collections.remove('overall')
     return render_template('index.html', 
                         metrics=metrics, 
                         metric_names=metric_names,
                         metric_labels=metric_labels,
-                        type_labels=type_labels)
+                        type_labels=type_labels,
+                        modules=collections)
 
-@app.route('/<module_name>')
-def show(module_name):
-    return render_template('module.html')
+@app.route('/module/<path:module_name>')
+def module(module_name):
+    client = MongoClient()
+    db = client.research
+    module_names = db.collection_names()
+    if module_name not in module_names:
+        flash('Module [%s] not found' % module_name, 'danger')
+        return redirect(url_for('list_modules'))
+    cursor = db[module_name].find()
+    metrics = {}
+    for m in cursor:
+        metrics[m['name']] = m
+    metric_names = [
+        ('top_n_commit_count', 'top_n_commit_ratio'),
+        ('latency_median', 'latency_mean'),
+        ('non_contiguous_pressure', 'contiguous_pressure'),
+        ('max_file_complexity', 'max_contact_complexity'),
+    ]
+
+    metric_labels = {
+        'top_n_commit_count': u'前n提交者提交数量',
+        'top_n_commit_ratio': u'前n提交者提交比例',
+        'latency_median': u'延迟中值',
+        'latency_mean': u'延迟均值',
+        'non_contiguous_pressure': u'压力（非连续）',
+        'contiguous_pressure': u'压力（连续）',
+        'max_file_complexity': u'最大文件复杂度',
+        'max_contact_complexity': u'最大人员复杂度',
+    }
+    
+    collections = db.collection_names()
+    if 'overall' in collections:
+        collections.remove('overall')
+    return render_template('module.html',
+                            module_name=module_name,
+                            metrics=metrics,
+                            metric_names=metric_names,
+                            metric_labels=metric_labels,
+                            modules=collections)
+
+@app.route('/modules')
+def list_modules():
+    def group_by_init(collections):
+        alpha = defaultdict(list)
+        other = []
+        for c in collections:
+            if c[0].isalpha():
+                alpha[c[0]].append(c)
+            else:
+                other.append(c)
+        return alpha, other
+
+    client = MongoClient()
+    db = client.research
+    collections = db.collection_names()
+    if 'overall' in collections:
+        collections.remove('overall')
+    alpha, other = group_by_init(collections)
+    return render_template('all_modules.html', alpha=alpha, other=other, modules=collections)
