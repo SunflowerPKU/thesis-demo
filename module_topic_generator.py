2769a780dcd7e68544780ee97dd36108fa3b2b30
Qimu Zheng <zheng.qm@163.com>
2018-04-15 19:59:46 +0800

    80%

---
+++ module_topic_generator.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import logging
+
+from maintainer_file_parser import *
+from module_metric_calculator import *
+from pymongo import MongoClient
+from stat_analyzer import *
+
+def insert_metric(db, module_name, result, terms):
+    keys = sorted(result.keys())
+    data = {
+        'name': 'topic',
+        'keys': keys,
+        'topics': {
+            '0' : map(lambda k: result[k][0], keys),
+            '1' : map(lambda k: result[k][1], keys),
+            '2' : map(lambda k: result[k][2], keys),
+            '3' : map(lambda k: result[k][3], keys),
+            '4' : map(lambda k: result[k][4], keys),
+        },
+        'terms' : {
+            '0' : terms[0],
+            '1' : terms[1],
+            '2' : terms[2],
+            '3' : terms[3],
+            '4' : terms[4],
+        }
+    }
+    db[module_name].insert_one(data)
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG, filename='module_topic.log')
+
+    client = MongoClient()
+    db = client.research
+
+    logging.info('Reading file commits.pkl')
+    df = pd.read_pickle("../linux_research/commits.pkl")
+    logging.info("File commits.pkl loaded successfully")
+
+    logging.info("Reading and parsing MAINTAINER")
+    with open("MAINTAINERS") as f:
+        maintainer_text = f.read()
+    modules = parse(maintainer_text)
+    logging.info("MAINTAINER info loaded")
+
+    for module_name, info in modules.items():
+        # if module_name != 'INTEL DRM DRIVERS (excluding Poulsbo, Moorestown and derivative chipsets)':
+        #     continue
+        try:
+            if 'M' not in info:
+                continue
+            if 'F' not in info:
+                continue
+            if ('S' not in info) or (info['S'][0] not in ['Supported', 'Maintained']):
+                continue
+                
+            logging.info("Processing collection " + module_name)
+            commits = commit_filter(df, info)
+            by_month = commits.committed_datetime.map(lambda t: t.year*  100 + t.month).value_counts()
+            if len(by_month) != 54 or by_month.min() < 10:
+                continue
+            logging.info(module_name + " meet the topic modeling requirements")
+            topic_distribution, terms = lda(commits.message)
+
+            combined =  pd.concat([topic_distribution, commits[['committed_datetime']]], axis=1)
+            result = {}
+            for year in range(2013, 2018):
+                for month in range(1,13):
+                    if year == 2017 and month == 7:
+                        continue
+                    selected = combined[combined.committed_datetime.map(lambda t: t.year == year and t.month == month)]
+                    topics = selected[[i for i in range(5)]].mean()
+                    result[(year, month)] = topics
+            insert_metric(db, module_name, result, terms)
+
+        except Exception, e:
+            logging.warning("In module " + module_name)
+            logging.warning(info)
+            logging.warning(e)
+            continue
+
+
--- stat_analyzer.py
+++ stat_analyzer.py
@@ -1,6 +1,12 @@
-import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
 
+import gensim
+import pandas as pd
 import statsmodels.api as sm
+
 
 def linear_regression(x, y):
     X = pd.DataFrame({'ts':x, 'const': [1.0] * len(x)}, index=x)
@@ -9,3 +15,33 @@ def linear_regression(x, y):
     slope = res.params['ts']
     pvalue = res.pvalues['ts']
     return slope, pvalue
+
+def lda(messages, num_topics=5):
+    tokenizer = RegexpTokenizer(r'\w+')
+    en_stop = get_stop_words('en')
+    p_stemmer = PorterStemmer()
+
+    def preprocess(message):
+        tokens = tokenizer.tokenize(message.lower())
+        stopped_tokens = [i for i in tokens if (not i in en_stop) and (not i.isdigit()) and (len(i) > 2)]
+        stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+        return stemmed_tokens
+
+    texts = map(preprocess, list(messages))
+    dictionary = corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    tfidf = models.TfidfModel(corpus, id2word=dictionary)
+    vec = [v for k,v in tfidf.idfs.items()]
+    low_value = 0.02
+    low_value_words = []
+    for bow in corpus:
+        low_value_words += [id for id, value in tfidf[bow] if value < low_value]
+    dictionary.filter_tokens(bad_ids=low_value_words)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+
+
+    ldamodel = models.ldamodel.LdaModel(corpus, num_topics=num_topics, id2word = dictionary, passes=50)
+    topic_distribution = map(lambda c: map(lambda t: t[1], ldamodel.get_document_topics(c, minimum_probability=0)), corpus)
+    topic_distribution = pd.DataFrame(topic_distribution, index=messages.index)
+    terms = map(lambda idx: map(lambda t: t[0], ldamodel.show_topic(idx, 5)), range(num_topics))
+    return topic_distribution, terms
